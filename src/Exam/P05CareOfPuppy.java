c20f0ad3537fca064ea7f82a4945ef1553e83465
Boris <b.todorov91@gmail.com>
2022-09-07 13:09:42 +0300

    ne up

---
+++ src/Exam/P05CareOfPuppy.java
@@ -0,0 +1,26 @@
+package Exam;
+
+import java.util.Scanner;
+
+public class P05CareOfPuppy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int neededFood = Integer.parseInt(scanner.nextLine());
+        String comand = scanner.nextLine();
+
+        int sum = 0;
+        while (!comand.equals("Adopted")){
+            int food = Integer.parseInt(comand);
+            sum += food;
+            comand = scanner.nextLine();
+        }
+        int totalFood = neededFood * 1000;
+
+        if (totalFood >= sum){
+            System.out.printf("Food is enough! Leftovers: %d grams.", totalFood - sum);
+        }else {
+            System.out.printf("Food is not enough. You need %d grams more.", sum - totalFood);
+        }
+    }
+}
